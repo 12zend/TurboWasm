a1a7c17dd3ed5299470872ac6ef7a443d8ff49b3
12zend <12zend.scratch@gmail.com>
2026-04-03 09:45:47 +0900

    a

--- scratch-vm-develop/webpack.config.js
+++ scratch-vm-develop/webpack.config.js
@@ -2,14 +2,46 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const defaultsDeep = require('lodash.defaultsdeep');
 const path = require('path');
 
+const isBuild = process.env.npm_lifecycle_event === 'build';
+
+class DuplicateAssetPlugin {
+    constructor (from, to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    apply (compiler) {
+        compiler.hooks.emit.tap('DuplicateAssetPlugin', compilation => {
+            const sourceAsset = compilation.assets[this.from];
+            if (!sourceAsset) {
+                return;
+            }
+
+            compilation.assets[this.to] = {
+                source: () => sourceAsset.source(),
+                size: () => sourceAsset.size()
+            };
+
+            const sourceMapAsset = compilation.assets[`${this.from}.map`];
+            if (sourceMapAsset) {
+                compilation.assets[`${this.to}.map`] = {
+                    source: () => sourceMapAsset.source(),
+                    size: () => sourceMapAsset.size()
+                };
+            }
+        });
+    }
+}
+
 const base = {
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    mode: 'development',
+    parallelism: isBuild ? 20 : undefined,
     devServer: {
         contentBase: false,
         host: '0.0.0.0',
         port: process.env.PORT || 8073
     },
-    devtool: 'cheap-module-source-map',
+    devtool: isBuild ? false : 'cheap-module-source-map',
     output: {
         library: 'VirtualMachine',
         filename: '[name].js'
@@ -37,14 +69,22 @@ const base = {
 module.exports = [
     // Web-compatible
     defaultsDeep({}, base, {
+        name: 'web',
         target: 'web',
         entry: {
-            'scratch-vm': './src/index.js',
-            'scratch-vm.min': './src/index.js'
+            'scratch-vm': './src/index.js'
         },
         output: {
             libraryTarget: 'umd',
             path: path.resolve('dist', 'web')
+        },
+        resolve: {
+            alias: {
+                '@turbowarp/scratch-svg-renderer$': path.resolve(
+                    __dirname,
+                    'node_modules/@turbowarp/scratch-svg-renderer/dist/web/scratch-svg-renderer.js'
+                )
+            }
         },
         module: {
             rules: base.module.rules.concat([
@@ -53,10 +93,15 @@ module.exports = [
                     loader: 'expose-loader?VirtualMachine'
                 }
             ])
-        }
+        },
+        plugins: base.plugins.concat([
+            new DuplicateAssetPlugin('scratch-vm.js', 'scratch-vm.min.js')
+        ])
     }),
     // Node-compatible
     defaultsDeep({}, base, {
+        name: 'node',
+        dependencies: ['web'],
         target: 'node',
         entry: {
             'scratch-vm': './src/index.js'
@@ -76,6 +121,8 @@ module.exports = [
     }),
     // Playground
     defaultsDeep({}, base, {
+        name: 'playground',
+        dependencies: ['node'],
         target: 'web',
         entry: {
             'benchmark': './src/playground/benchmark',
@@ -84,6 +131,11 @@ module.exports = [
         output: {
             path: path.resolve(__dirname, 'playground'),
             filename: '[name].js'
+        },
+        externals: {
+            'scratch-render': 'ScratchRender',
+            'scratch-storage': 'ScratchStorage',
+            '@turbowarp/scratch-svg-renderer': 'ScratchSVGRenderer'
         },
         module: {
             rules: base.module.rules.concat([
@@ -106,14 +158,6 @@ module.exports = [
                 {
                     test: require.resolve('scratch-audio/src/index.js'),
                     loader: 'expose-loader?AudioEngine'
-                },
-                {
-                    test: require.resolve('scratch-storage/src/index.js'),
-                    loader: 'expose-loader?ScratchStorage'
-                },
-                {
-                    test: require.resolve('scratch-render/src/index.js'),
-                    loader: 'expose-loader?ScratchRender'
                 }
             ])
         },
