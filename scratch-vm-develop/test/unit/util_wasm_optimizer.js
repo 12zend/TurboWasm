20c49e234a274b7725ea8ea9994dbf6346283598
12zend <12zend.scratch@gmail.com>
2026-04-03 14:35:25 +0900

    a

--- scratch-vm-develop/test/unit/util_wasm_optimizer.js
+++
@@ -1,9 +0,0 @@
-const test = require('tap').test;
-const wasmOptimizer = require('../../src/util/wasm-optimizer');
-
-test('wasm optimizer initializes a valid module', async t => {
-    await wasmOptimizer.init();
-    t.equal(wasmOptimizer.ready, true);
-    t.equal(wasmOptimizer.toNumber(4.5), 4.5);
-    t.equal(wasmOptimizer.toNumber(NaN), 0);
-});
